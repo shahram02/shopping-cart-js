ee07dfd35aeee9be1de6fe762869c000f4aeccb0
shahram <shahramkhaleh2001@gmail.com>
2022-07-02 13:18:22 -0700

    End Total Price Cart

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ import { productsData } from "./products.js";
 
 // DOM tags
 const productsContainer = document.querySelector(".products-center");
+const cartItem = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+
+let cart = [];
 
 // Get Products
 class Products {
@@ -24,12 +28,42 @@ class ShowUi {
         <p class="product-price">تومان ${product.price}</p>
         <p class="product-title">${product.title}</p>
       </div>
-      <button class="btn add-to-cart" ${product.id}>
+      <button class="btn add-to-cart" data-id=${product.id}>
       اضافه کردن محصول
       </button>
     </div>`;
       productsContainer.innerHTML = result;
     });
+  }
+  getBtns() {
+    const addToCarBtn = document.querySelectorAll(".add-to-cart");
+    const buttons = [...addToCarBtn];
+    buttons.forEach((button) => {
+      const id = button.dataset.id;
+      const isInCart = cart.find((p) => p.id === id);
+      if (isInCart) {
+        button.innerText = "در سبد خرید موجود هست";
+        button.disabled = true;
+      }
+      button.addEventListener("click", (e) => {
+        const addProduct = Storage.getProduct(id);
+        console.log(addProduct);
+        cart = [...cart, { ...addProduct, quantity: 1 }];
+        Storage.saveCarts(cart);
+        e.target.innerText = "در سبد خرید موجود هست";
+        e.target.disabled = true;
+        this.setCartValue(cart);
+      });
+    });
+  }
+  setCartValue(cart) {
+    let totalItems = 0;
+    const totalPrice = cart.reduce((acc, curr) => {
+      totalItems += curr.quantity;
+      return acc + curr.quantity * curr.price;
+    }, 0);
+    cartItem.innerText = totalItems;
+    cartTotal.innerHTML = `مجموع قیمت${totalPrice.toFixed(2)} تومان`;
   }
 }
 
@@ -38,6 +72,15 @@ class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
   }
+  static getProduct(id) {
+    console.log(id);
+    const products = JSON.parse(localStorage.getItem("products"));
+    return products.find((p) => p.id == parseInt(id));
+  }
+  static saveCarts(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  static getCart() {}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -46,6 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const dataProducts = products.getPtoducts();
   const showUi = new ShowUi();
   showUi.addToDOM(dataProducts);
+  showUi.getBtns();
 
   //   Storage
   Storage.saveProducts(productsData);
